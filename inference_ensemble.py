e37afbba35eb4110a892074110bacda51fd8744a
tewodros s. gebre <107203369+tsgebre@users.noreply.github.com>
2026-04-23 18:33:33 -0400

    Add files via upload

---
+++ inference_ensemble.py
@@ -0,0 +1,5 @@
+import os
+import torch
+import numpy as np
+# Skeleton script for Deep Ensembles
+print('Inference ensemble script ready to be implemented. Load models, compute mean of variances (Aleatoric) and variance of means (Epistemic).')
